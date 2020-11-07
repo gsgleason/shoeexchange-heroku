9be0b684accc53018bf5ce596bc4b788d341e6e8
Gregory Gleason <gsgleason@gmail.com>
2020-11-06 20:12:17 -0700

    added db to app

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify, request
+from db import DB
 
 app = Flask(__name__)
 
+db = DB("listing_id", "listings")
+
 @app.route('/')
-def index():
-    return Response('Hello there.')
+def list():
+    return jsonify(db.fetchall())
+
+@app.route('/<item_id>')
+def get(item_id):
+    return jsonify(db.fetch(item_id))
+
+@app.route('/')
+def add():
+    db.add(request.json)
+    return Response(status=201)
+
+@app.route('/<item_id>', methods=['DELETE'])
+def delete(item_id):
+    db.delete(item_id)
+    return Response(status=204)
+
