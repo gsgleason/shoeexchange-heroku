c85bea2915340ff098bf4b0909c49ac3a51b3654
Gregory Gleason <gsgleason@gmail.com>
2020-11-08 17:53:31 -0700

    replaced configparser config with env var for secrets

--- reddit.py
+++ reddit.py
@@ -1,11 +1,11 @@
 import praw
-from config import config
+import os
 
 def get_reddit():
     return praw.Reddit(
-            user_agent=config['praw']['user_agent'],
-            client_id=config['praw']['client_id'],
-            client_secret=config['praw']['client_secret'],
-            username=config['praw']['username'],
-            password=config['praw']['password']
+            user_agent=os.environ.get("PRAW_USER_AGENT"),
+            client_id=os.environ.get("PRAW_CLIENT_ID"),
+            client_secret=os.environ.get("PRAW_CLIENT_SECRET"),
+            username=os.environ.get("PRAW_USERNAME"),
+            password=os.environ.get("PRAW_PASSWORD")
             )
