f38000a0bef6f8be42066e78d822a384219ae020
Gregory Gleason <gsgleason@gmail.com>
2020-11-09 08:51:49 -0700

    trying to pass int() to jinja

--- app.py
+++ app.py
@@ -9,6 +9,5 @@ db = DB("submission_id", "listings")
 @app.route('/')
 def index():
     listings = db.fetchall()
-    last_updated = datetime.fromtimestamp(listings[0]['updated_utc']).isoformat()
-    return render_template('index.html', listings=listings, datetime=datetime)
+    return render_template('index.html', listings=listings, datetime=datetime, int=int)
 
