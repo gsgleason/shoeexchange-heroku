973bd11aa00a9ec9cf04947e930396552e745bb5
Gregory Gleason <gsgleason@gmail.com>
2020-11-09 08:36:43 -0700

    passing datetime to template to parse timestamps

--- app.py
+++ app.py
@@ -1,25 +1,14 @@
-from flask import Flask, Response, jsonify, request
+from flask import Flask, render_template
 from db import DB
+from datetime import datetime
 
 app = Flask(__name__)
 
 db = DB("submission_id", "listings")
 
 @app.route('/')
-def list():
-    return jsonify(db.fetchall())
-
-@app.route('/<item_id>')
-def get(item_id):
-    return jsonify(db.fetch(item_id))
-
-@app.route('/', methods=['POST'])
-def add():
-    db.add(request.json)
-    return Response(status=201)
-
-@app.route('/<item_id>', methods=['DELETE'])
-def delete(item_id):
-    db.delete(item_id)
-    return Response(status=204)
+def index():
+    listings = db.fetchall()
+    last_updated = datetime.fromtimestamp(listings[0]['updated_utc']).isoformat()
+    return render_template('index.html', listings=listings, datetime=datetime)
 
